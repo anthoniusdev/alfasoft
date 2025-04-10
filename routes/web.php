dfd0d5a6b63ff363172dc7d219667e36f1df19dd
Anthonius Souza <anthoniusmiguel@gmail.com>
2025-04-10 13:03:43 -0300

    feat: add routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [\App\Http\Controllers\ContactController::class, 'index'])->name('home');
+Route::get('/home', [\App\Http\Controllers\ContactController::class, 'index'])->name('home');
+Route::middleware('auth')->group(function () {
+    Route::prefix('contacts')->group(function () {
+        Route::get('/create', [\App\Http\Controllers\ContactController::class, 'create'])->name('contacts.create');
+        Route::post('/store', [\App\Http\Controllers\ContactController::class, 'store'])->name('contacts.store');
+        Route::get('/show/{id}', [\App\Http\Controllers\ContactController::class, 'show'])->name('contacts.show');
+        Route::get('/edit/{id}', [\App\Http\Controllers\ContactController::class, 'edit'])->name('contacts.edit');
+        Route::put('/update/{id}', [\App\Http\Controllers\ContactController::class, 'update'])->name('contacts.update');
+        Route::delete('/destroy/{id}', [\App\Http\Controllers\ContactController::class, 'destroy'])->name('contacts.destroy');
+    });
 });
+
+Auth::routes();
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
